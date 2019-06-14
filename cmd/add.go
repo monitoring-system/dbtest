04401e8ddfa38973b52e7be0c8dc0257fcfa483d
sdojjy <sdojjy@qq.com>
2019-06-14 18:06:31 +0800

    change command

--- cmd/add.go
+++ cmd/add.go
@@ -4,11 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/monitoring-system/dbtest/api/types"
+	"github.com/prometheus/common/log"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
+	"path"
 	"strings"
+	"time"
 )
 
 var AddTestCmd = &cobra.Command{
@@ -17,7 +21,11 @@ var AddTestCmd = &cobra.Command{
 	Long:  "add a test to the db test web server",
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		addTest()
+		if loaderPath == "" {
+			addTest()
+		} else {
+			LoadRandgenConf()
+		}
 	},
 }
 var yyFile string
@@ -28,6 +36,7 @@ var queryCount int
 var queryLoader string
 var dataLoader string
 var name string
+var loaderPath string
 
 func init() {
 	AddTestCmd.Flags().StringVar(&yyFile, "yy", "", "randgen yy file path")
@@ -38,21 +47,22 @@ func init() {
 	AddTestCmd.Flags().StringVar(&queryLoader, "query-loader", "randgen", "query loaders, split by comma")
 	AddTestCmd.Flags().StringVar(&dataLoader, "data-loader", "randgen", "data loaders, split by comma")
 	AddTestCmd.Flags().StringVar(&name, "name", "console", "data loaders, split by comma")
+	AddTestCmd.Flags().StringVar(&loaderPath, "loadpath", "", "randgen yy/zz directory path")
 }
 
 type randgenConf struct {
 	yyFile string
 	zzFile string
-	loop int
+	loop   int
 }
 
 type RandgenConfOpt struct {
-	conf randgenConf
+	conf         randgenConf
 	LoopInterval int
-	QueryCount int
-	QueryLoader string
-	DataLoader string
-	Name string
+	QueryCount   int
+	QueryLoader  string
+	DataLoader   string
+	Name         string
 }
 
 func NewRandgenConf(yyFile string, zzFile string, loop int) *RandgenConfOpt {
@@ -60,13 +70,13 @@ func NewRandgenConf(yyFile string, zzFile string, loop int) *RandgenConfOpt {
 		conf: randgenConf{
 			yyFile: yyFile,
 			zzFile: zzFile,
-			loop: loop,
+			loop:   loop,
 		},
-		LoopInterval: 30,
-		QueryCount: 1000,
-		QueryLoader: "randgen",
-		DataLoader: "randgen",
-		Name: "console",
+		LoopInterval: loopInterval,
+		QueryCount:   queryCount,
+		QueryLoader:  "randgen",
+		DataLoader:   "randgen",
+		Name:         "console",
 	}
 }
 
@@ -110,4 +120,67 @@ func addTest() {
 func getPayload(payload *types.Test) string {
 	bytes, _ := json.Marshal(payload)
 	return string(bytes)
+}
+
+func LoadRandgenConf() {
+	if loaderPath == "" {
+		return
+	}
+
+	for {
+		_, err := http.Get("http://localhost:8080")
+		if err == nil {
+			break
+		}
+
+		log.Warn("Wait server started:")
+		time.Sleep(time.Second)
+	}
+
+	infos, err := ioutil.ReadDir(loaderPath)
+	if err != nil {
+		log.Error("load conf files failed", zap.Error(err))
+		return
+	}
+
+	submmitedFiles := make(map[string]struct{})
+	for _, info := range infos {
+		valid, newName := rename(info.Name())
+		if !valid {
+			continue
+		}
+
+		if _, ok := submmitedFiles[newName]; ok {
+			continue
+		}
+		submmitedFiles[newName] = struct{}{}
+
+		yy := fmt.Sprintf("%s/%s", loaderPath, rebuildName(newName, "yy"))
+		zz := fmt.Sprintf("%s/%s", loaderPath, rebuildName(newName, "zz"))
+
+		if !PathExist(yy) || !PathExist(zz) {
+			log.Info(fmt.Sprintf("%s/%s", loaderPath, yy))
+			continue
+		}
+
+		log.Info("submit rangen file", zap.String("yy", yy), zap.String("zz", zz))
+		NewRandgenConf(yy, zz, loop).Add()
+	}
+}
+
+func PathExist(_path string) bool {
+	_, err := os.Stat(_path)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func rebuildName(name string, suffix string) string {
+	return fmt.Sprintf("%s.%s", name, suffix)
+}
+
+func rename(fileName string) (bool, string) {
+	ext := path.Ext(fileName)
+	return ext == ".zz" || ext == ".yy", strings.TrimSuffix(fileName, ext)
 }
